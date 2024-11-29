a39269235b76f9ece576f8196867d307fa205555
Niraj More <moreniraj49@gmail.com>
2024-11-29 21:24:10 +0530

    changed origin

--- controllers/shop/order-controller.js
+++ controllers/shop/order-controller.js
@@ -26,8 +26,8 @@ const createOrder = async (req, res) => {
         payment_method: "paypal",
       },
       redirect_urls: {
-        return_url: "https://ecommerce-frontend-phi-teal.vercel.app/:5173/shop/paypal-return",
-        cancel_url: "https://ecommerce-frontend-phi-teal.vercel.app//shop/paypal-cancel",
+        return_url: "https://ecommerce-frontend-phi-teal.vercel.app/shop/paypal-return",
+        cancel_url: "https://ecommerce-frontend-phi-teal.vercel.app/shop/paypal-cancel",
       },
       transactions: [
         {
